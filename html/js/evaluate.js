22f7b409ff5dcbbfe41320d93d9e93b561b75e63
zhouhanwen <zhouhanwen@kedacom.com>
2019-08-14 22:09:57 +0800

    样式修改

--- html/js/evaluate.js
+++ html/js/evaluate.js
@@ -2,224 +2,105 @@ var blueFlower = 0;
 var redFlower = 0;
 var evaluateType = 1; // 评价类型 1：行为习惯2：学习习惯
 
-function backHome() {
-    window.location.href = 'home.html?RowGuid=' + evaluateApi.ClassGuid;
+function backHome () {
+  window.location.href = window.location.href.replace('evaluate.html', 'home.html');
 }
 
-function getBule(num, state) {
-    let arr = $('.blueFlower');
-    if (num === 1 && blueFlower === 1 && arr[0].src.match('icon_hua_blue.png')) {
-        blueFlower = 0;
-        arr[0].src = 'assets/icon_hua_gray.png';
+function getBule (num, state) {
+  let arr = $('.blueFlower');
+  if (num === 1 && blueFlower === 1 && arr[0].src.match('icon_hua_blue.png')) {
+    blueFlower = 0;
+    arr[0].src = 'assets/icon_hua_gray.png';
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (i <= num - 1) {
+        arr[i].src = 'assets/icon_hua_blue.png';
+      } else {
+        arr[i].src = 'assets/icon_hua_gray.png';
+      }
+    }
+    blueFlower = num;
+  }
+  if (!state) {
+    getRed(0, true);
+  }
+}
+
+function getRed (num, state) {
+  let arr = $('.redFlower');
+  if (num === 1 && redFlower === 1 && arr[0].src.match('icon_hua_red.png')) {
+    redFlower = 0;
+    arr[0].src = 'assets/icon_hua_gray.png';
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (i <= num - 1) {
+        arr[i].src = 'assets/icon_hua_red.png';
+      } else {
+        arr[i].src = 'assets/icon_hua_gray.png';
+      }
+    }
+    redFlower = num;
+  }
+  if (!state) {
+    getBule(0, true);
+  }
+}
+
+function chooseType (type) {
+  evaluateType = type;
+  if (type === 1) {
+    $('.module1')[0].children[0].children[0].src = 'assets/btn_xuanzhong.png';
+    $('.module1')[0].children[1].children[0].src = 'assets/icon_quan.png';
+  } else if (type === 2) {
+    $('.module1')[0].children[1].children[0].src = 'assets/btn_xuanzhong.png';
+    $('.module1')[0].children[0].children[0].src = 'assets/icon_quan.png';
+  }
+}
+
+function addStudent (event, el, student) {
+  event.stopPropagation();
+  event.preventDefault();
+  if (el.className === '') {
+    el.className = 'activeLi';
+    if ($('#textareaStudent')[0].value) {
+      $('#textareaStudent')[0].value = $('#textareaStudent')[0].value + ';' + student;
     } else {
-        for (let i = 0; i < arr.length; i++) {
-            if (i <= num - 1) {
-                arr[i].src = 'assets/icon_hua_blue.png';
-            } else {
-                arr[i].src = 'assets/icon_hua_gray.png';
-            }
-        }
-        blueFlower = num;
+      $('#textareaStudent')[0].value = student;
     }
-    if (!state) {
-        getRed(0, true);
-    }
+  } else {
+    el.className = '';
+    $('#textareaStudent')[0].value = $('#textareaStudent')[0].value.replace(';' + student, '').replace(student, '');
+  }
 }
 
-function getRed(num, state) {
-    let arr = $('.redFlower');
-    if (num === 1 && redFlower === 1 && arr[0].src.match('icon_hua_red.png')) {
-        redFlower = 0;
-        arr[0].src = 'assets/icon_hua_gray.png';
-    } else {
-        for (let i = 0; i < arr.length; i++) {
-            if (i <= num - 1) {
-                arr[i].src = 'assets/icon_hua_red.png';
-            } else {
-                arr[i].src = 'assets/icon_hua_gray.png';
-            }
-        }
-        redFlower = num;
-    }
-    if (!state) {
-        getBule(0, true);
-    }
+function sure () {
+  var data = {
+    blueFlower: blueFlower,
+    redFlower: redFlower,
+    evaluateType: evaluateType,
+    student: $('#textareaStudent')[0].value.split(';'),
+  };
+  console.log('提交的数据', data);
 }
 
-function chooseType(type, evaluateType) {
-    evaluateApi.evaluateType = evaluateType;
-    if (type === 1) {
-        $('.module1')[0].children[0].children[0].src = 'assets/btn_xuanzhong.png';
-        $('.module1')[0].children[1].children[0].src = 'assets/icon_quan.png';
-    } else if (type === 2) {
-        $('.module1')[0].children[1].children[0].src = 'assets/btn_xuanzhong.png';
-        $('.module1')[0].children[0].children[0].src = 'assets/icon_quan.png';
-    }
+function clearData () {
+  getBule(0);
+  getRed(0);
+  chooseType(1);
+  $('#textareaStudent')[0].value = '';
 }
 
-function addStudent(el, student,rowguid) {
-    if (el.className === '') {
-        el.className = 'activeLi';
-        if ($('#textareaStudent')[0].value) {
-            $('#textareaStudent')[0].value = $('#textareaStudent')[0].value + ';' + student;
-
-            evaluateApi.SelectStudent += ';' + rowguid;
-        } else {
-            $('#textareaStudent')[0].value = student;
-
-            evaluateApi.SelectStudent = rowguid;
-        }
-    } else {
-        el.className = '';
-        $('#textareaStudent')[0].value = $('#textareaStudent')[0].value.replace(';' + student, '').replace(student, '').replace(new RegExp('^\\;+', 'g'), '');
-
-        evaluateApi.SelectStudent = evaluateApi.SelectStudent.replace(';' + rowguid, '').replace(rowguid, '');
-    }
+function changeStudentDiv (event) {
+  event.stopPropagation();
+  event.preventDefault();
+  let el = $('#dropBut').next()[0];
+  if (el.style.display === 'block') {
+    el.style.display = 'none';
+  } else if (el.style.display === 'none') {
+    el.style.display = 'block';
+  }
 }
 
-function sure() {
-    if (evaluateApi.SelectStudent == "") {
-        layer.alert("请选择学生！");
-        retrun;
-    }
-    if (blueFlower == 0 && redFlower == 0) {
-        layer.alert("请选择红蓝花！");
-        retrun;
-    }
-    var data = {
-        blueFlower: blueFlower,
-        redFlower: redFlower,
-        evaluateType: evaluateApi.evaluateType,
-        student: evaluateApi.SelectStudent.split(';'),
-        TeacherGuid: evaluateApi.UserGuid
-    };
-    evaluateApi.Submit(data);
-}
-
-function clearData() {
-    getBule(0);
-    getRed(0);
-    chooseType(1, '行为习惯');
-    $("#studentlist li").removeClass("activeLi");
-    evaluateApi.SelectStudent = "";
-    $('#textareaStudent')[0].value = '';
-}
-
-function changeStudentDiv() {
-    let el = $('#dropBut').next()[0];
-    if (el.style.display === 'block') {
-        el.style.display = 'none';
-    } else if (el.style.display === 'none') {
-        el.style.display = 'block';
-    }
-}
-
-
-var evaluateApi = {
-    apiurl: Util.AjaxRUrl + "Class.ashx",
-    UserGuid: Util.getQueryString("UserGuid"),
-    ClassGuid: Util.getQueryString("RowGuid"),
-    SelectStudent: "",
-    timer:"",
-    evaluateType: '行为习惯',
-    StudentCode:"",
-    GetStudentList: function () {
-        jQuery.support.cors = true;
-        $.ajax({
-            type: "post",
-            url: this.apiurl,
-            data: {
-                "method": "getclassstudent",
-                "rowguid": this.ClassGuid
-            },
-            dataType: "json",
-            success: function (jsondata) {
-                if (jsondata.status == "success") {
-                    evaluateApi.BindTemplate(jsondata.data,
-                        $("#studentlist"),
-                        $("#studentlist-Item"));
-                } else {
-                    layer.alert(jsondata.status);
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) { layer.alert("getclassstudent=" + errorThrown); }
-        });
-    },
-    //绑定模板
-    BindTemplate: function (jsondata, $list, $item) {
-        var M = Mustache, activitylistTmpl = $item.html();
-
-        $list.html(M.render(activitylistTmpl, jsondata));
-    },
-    Submit: function (submitdata) {
-        jQuery.support.cors = true;
-        $.ajax({
-            type: "post",
-            url: this.apiurl,
-            data: {
-                "method": "submit",
-                "submitdata": JSON.stringify(submitdata)
-            },
-            dataType: "json",
-            success: function (jsondata) {
-                if (jsondata.status == "success") {
-                    layer.alert('评价成功！', function () {
-                        backHome();
-                    });
-                } else {
-                    layer.alert(jsondata.status);
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) { layer.alert("submit=" + errorThrown); }
-        });
-    },
-    GetStudentInfo: function (code) {
-        jQuery.support.cors = true;
-        $.ajax({
-            type: "post",
-            url: this.apiurl,
-            data: {
-                "method": "getstudentinfo",
-                "code": code,
-                "classguid": this.ClassGuid
-            },
-            dataType: "json",
-            success: function (jsondata) {
-                if (jsondata.status == "success") {
-                    //下拉菜单联动
-                    $("#" + jsondata.data.RowGuid).click();
-                } else {
-                    layer.alert(jsondata.status);
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) { layer.alert("submit=" + errorThrown); }
-        });
-    },
-    GetLocalTime: function () {
-        var mydate = new Date();
-        $("#day").text(mydate.toLocaleDateString());
-        var hour = mydate.getHours();
-        var minute = mydate.getMinutes();
-        $("#time").text(hour + ":" + minute);
-    }
-}
-
-$(function () {
-    evaluateApi.GetStudentList();
-    evaluateApi.GetLocalTime();
-
-    $(document).keydown(function (event) {
-        if (event.keyCode == "13") {
-            //回车执行查询
-            evaluateApi.GetStudentInfo(evaluateApi.StudentCode);
-            evaluateApi.StudentCode = "";
-            layer.closeAll();
-        } else {
-            evaluateApi.StudentCode += String.fromCharCode(event.keyCode);
-        }
-    });
-    
-    evaluateApi.timer = setInterval(function () {
-        evaluateApi.GetLocalTime();
-    }, 30000);
-});
+document.onclick = function() {
+  $('#dropBut').next()[0].style.display = 'none';
+};
--- html/js/home.js
+++ html/js/home.js
@@ -1,245 +1,50 @@
-function openImgModule() {
-    $('#imgModuleWrap').css('display', 'flex');
+function openImgModule () {
+  $('#imgModuleWrap').css('display', 'flex');
 }
 
-function backHome() {
-    $('#imgModuleWrap').css('display', 'none');
-    $('#evaluateModule').css('display', 'none');
+function backHome () {
+  $('#imgModuleWrap').css('display', 'none');
+  $('#evaluateModule').css('display', 'none');
 }
 
-function openEvaluateModule() {
-    $('#evaluateModule').css('display', 'flex');
-    $('#selectTeacherWrap').css('display', 'block');
-    $('#inputPasswordWrap').css('display', 'none');
+function openEvaluateModule () {
+  $('#evaluateModule').css('display', 'flex');
+  $('#selectTeacherWrap').css('display', 'block');
+  $('#inputPasswordWrap').css('display', 'none');
 }
 
-function chooseTeacher(id) {
-    let arr = $('.chooseTeacher');
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].getAttribute('data-teacherId') === id) {
-            homeapi.SelectedTeacherGuid = id;
-            arr[i].className = arr[i].className.replace('noActive', 'active');
-        } else {
-            arr[i].className = arr[i].className.replace('active', 'noActive');
-        }
+function chooseTeacher (id) {
+  let arr = $('.chooseTeacher');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].getAttribute('data-teacherId') === id) {
+      arr[i].className = arr[i].className.replace('noActive', 'active');
+    } else {
+      arr[i].className = arr[i].className.replace('active', 'noActive');
     }
+  }
 }
 
-function evaluateSure() {
-    if ($('#selectTeacherWrap')[0].style.display === 'block') {
-        //先判断是否选择教师
-        if (homeapi.SelectedTeacherGuid === "") {
-            layer.alert("请选择教师");
-        } else {
-            // 选择老师
-            $('#selectTeacherWrap').css('display', 'none');
-            $('#inputPasswordWrap').css('display', 'flex');
-        }
-    } else if ($('#inputPasswordWrap')[0].style.display === 'flex') {   
-        homeapi.CheckTeacherInfo($('#inputPassword')[0].value);
-        //// 输入密码
-        //console.log($('#inputPassword')[0].value);
-        //window.location.href = window.location.href.replace('home.html', 'evaluate.html');        
-    }
+function evaluateSure () {
+  if ($('#selectTeacherWrap')[0].style.display === 'block') {
+    // 选择老师
+    $('#selectTeacherWrap').css('display', 'none');
+    $('#inputPasswordWrap').css('display', 'flex');
+  } else if ($('#inputPasswordWrap')[0].style.display === 'flex') {
+    // 输入密码
+    console.log($('#inputPassword')[0].value);
+    window.location.href = window.location.href.replace('home.html', 'evaluate.html');
+  }
 }
 
-function changeRank(el, type) {
-    el.className = 'divActive';
-    if (type === 1) {
-        el.nextElementSibling.className = '';
-        $('#weekRank').css('display', 'flex');
-        $('#monthRank').css('display', 'none');
-    } else if (type === 2) {
-        el.previousElementSibling.className = '';
-        $('#weekRank').css('display', 'none');
-        $('#monthRank').css('display', 'flex');
-    }
+function changeRank (el, type) {
+  el.className = 'divActive';
+  if (type === 1) {
+    el.nextElementSibling.className = '';
+    $('#weekRank').css('display', 'flex');
+    $('#mouthRank').css('display', 'none');
+  } else if (type === 2) {
+    el.previousElementSibling.className = '';
+    $('#weekRank').css('display', 'none');
+    $('#mouthRank').css('display', 'flex');
+  }
 }
-
-var homeapi = {
-    apiurl: Util.AjaxRUrl + "Class.ashx",
-    ClassGuid: Util.getQueryString("RowGuid"),
-    SelectedTeacherGuid: "",
-    timer: null,
-    weathertimer: null,
-    kbjftimer:null,
-    GetClassDetail: function () {
-        jQuery.support.cors = true;
-        $.ajax({
-            type: "post",
-            url: this.apiurl,
-            data: {
-                "method": "getclassdetail",
-                "rowguid": this.ClassGuid
-            },
-            dataType: "json",
-            success: function (jsondata) {
-                if (jsondata.status == "success") {
-                    $("#ClassName").text(jsondata.data.ClassName);
-                    $("#StudentCount").text(jsondata.data.StudentCount);
-                    $("#BZRName").text(jsondata.data.BZRName);
-                    $("#FBZRName").text(jsondata.data.FBZRName);
-                    $("img[name='PhotoFileName']").attr("src", jsondata.data.PhotoFileName);
-
-                    homeapi.BindTemplate(jsondata.data,
-                        $("#rk"),
-                        $("#rk-Item"));
-                    homeapi.BindTemplate(jsondata.data,
-                        $("#selectTeacherWrap"),
-                        $("#rk1-Item"));
-                } else {
-                    layer.alert(jsondata.status);
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) { layer.alert("getclassdetail=" + errorThrown); }
-        });
-    },
-    //绑定模板
-    BindTemplate: function (jsondata, $list, $item) {
-        var M = Mustache, activitylistTmpl = $item.html();
-
-        $list.html(M.render(activitylistTmpl, jsondata));
-    },
-    GetClassKB: function () {
-        jQuery.support.cors = true;
-        $.ajax({
-            type: "post",
-            url: this.apiurl,
-            data: {
-                "method": "getclasskcb",
-                "rowguid": this.ClassGuid
-            },
-            dataType: "json",
-            success: function (jsondata) {
-                if (jsondata.status == "success") {
-                    homeapi.BindTemplate(jsondata.data,
-                        $("#kb"),
-                        $("#kb-Item"));
-                } else {
-                    layer.alert(jsondata.status);
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) { layer.alert("getclasskcb=" + errorThrown); }
-        });
-    },
-    GetNotice: function () {
-        jQuery.support.cors = true;
-        $.ajax({
-            type: "post",
-            url: this.apiurl,
-            data: {
-                "method": "getnotice"
-            },
-            dataType: "json",
-            success: function (jsondata) {
-                if (jsondata.status == "success") {
-                    $("#Notice").html(jsondata.data.Notice);
-                    $("#PublishTime").html(jsondata.data.PublishTime);
-                } else {
-                    layer.alert(jsondata.status);
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) { layer.alert("getclassdetail=" + errorThrown); }
-        });
-    },
-    //验证教师密码
-    CheckTeacherInfo: function (password) {
-        jQuery.support.cors = true;
-        $.ajax({
-            type: "post",
-            url: this.apiurl,
-            data: {
-                "method": "checkteacherinfo",
-                "userguid": this.SelectedTeacherGuid,
-                "password": password
-            },
-            dataType: "json",
-            success: function (jsondata) {
-                if (jsondata.status == "success") {
-                    window.location.href = 'evaluate.html?UserGuid=' + homeapi.SelectedTeacherGuid + '&RowGuid=' + homeapi.ClassGuid;
-                } else {
-                    layer.alert(jsondata.status);
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) { layer.alert("checkteacherinfo=" + errorThrown); }
-        });
-    },
-    GetScoreList: function () {
-        jQuery.support.cors = true;
-        $.ajax({
-            type: "post",
-            url: this.apiurl,
-            data: {
-                "method": "getscorelist",
-                "rowguid": this.ClassGuid
-            },
-            dataType: "json",
-            success: function (jsondata) {
-                if (jsondata.status == "success") {
-                    homeapi.BindTemplate(jsondata.data,
-                        $("#weekRank"),
-                        $("#week-Item"));
-                    homeapi.BindTemplate(jsondata.data,
-                        $("#monthRank"),
-                        $("#month-Item"));
-                } else {
-                    layer.alert(jsondata.status);
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) { layer.alert("getscorelist=" + errorThrown); }
-        });
-    },
-    GetLocalTime: function () {
-        var mydate = new Date();
-        $("#day").text(mydate.toLocaleDateString());
-        $("#day1").text(mydate.toLocaleDateString());
-        var hour = mydate.getHours();
-        var minute = mydate.getMinutes();
-        $("#time").text(hour + ":" + minute);
-    },
-    GetWeatherInfo: function () {
-        jQuery.support.cors = true;
-        $.ajax({
-            type: "get",
-            url: "https://www.tianqiapi.com/api/?version=v6&cityid=101290101",
-            data: {},
-            dataType: "json",
-            success: function (jsondata) {
-                //layer.alert(JSON.stringify(jsondata));
-                $("#tem").text(jsondata.tem);
-                $("#weather").html(jsondata.wea + "<span>|</span>" + jsondata.win + jsondata.win_speed + "<span>|</span>" + jsondata.city);
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) { layer.alert("getweatherinfo=" + errorThrown); }
-        });
-    }
-}
-
-$(function () {
-    //获取班级详情
-    homeapi.GetClassDetail();
-    //获取班级课表
-    homeapi.GetClassKB();
-    //获取通知
-    homeapi.GetNotice();
-    //获取积分列表
-    homeapi.GetScoreList();
-    //设定当前时间
-    homeapi.GetLocalTime();
-    //获取天气信息
-    homeapi.GetWeatherInfo();
-
-    //60s一次，更新时间
-    homeapi.timer = setInterval(function () {
-        
-        homeapi.GetLocalTime();
-    }, 60000);
-    //30分钟一次，更新天气和积分排名
-    homeapi.weathertimer = setInterval(function () {
-        homeapi.GetWeatherInfo();
-        homeapi.GetScoreList();
-    }, 1800000);
-    //3小时，更新课表
-    homeapi.kbjftimer = setInterval(function () {
-        homeapi.GetClassKB();
-    }, 10800000);
-});
